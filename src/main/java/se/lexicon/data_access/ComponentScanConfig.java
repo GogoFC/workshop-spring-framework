0556f0ff33982dd4889db727b0d49af01f5b2a81
rexmolo <jackie.hui.ma@gmail.com>
2024-02-07 10:51:12 +0100

    fixed

--- src/main/java/se/lexicon/data_access/ComponentScanConfig.java
+++ src/main/java/se/lexicon/data_access/ComponentScanConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("se.lexicon.data_access")
-public @interface ComponentScanConfig {
+@ComponentScan(basePackages = "se.lexicon")
+public class ComponentScanConfig {
 }
