dabd15b7d44356921dd5f74d85b181a78caac8ef
Goran M <goranmuminovic@gmail.com>
2024-02-07 11:09:42 +0100

    part 1 done

--- src/main/java/se/lexicon/model/Student.java
+++ src/main/java/se/lexicon/model/Student.java
@@ -1,4 +1,24 @@
 package se.lexicon.model;
 
 public class Student {
+
+    int id;
+    String name;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
